53ad1b2b7cc417c1108a8eb431effc9fe78e2b9b
Dallas Hogan <dhogan@maxmind.com>
2022-03-08 16:53:09 -0500

    Only installing Mac software on Mac

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 set -eux
 
-brew install go
+if [ “$(uname)” == “Darwin” ]; then
+    brew install go
 
-brew tap jandedobbeleer/oh-my-posh
-brew install oh-my-posh
+    brew tap jandedobbeleer/oh-my-posh
+    brew install oh-my-posh
+fi
 
 git config --global alias.doms 'diff -w -M origin/main...HEAD --stat --name-only'
