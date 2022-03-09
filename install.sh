b2add1471e4437b1cc8a9a19c3199904e2231cd6
Dallas Hogan <dhogan@maxmind.com>
2022-03-09 09:13:47 -0500

    Adding more git aliases

--- install.sh
+++ install.sh
@@ -9,4 +9,17 @@ if [ “$(uname)” == “Darwin” ]; then
     brew install oh-my-posh
 fi
 
+git config --global user.email "dhogan@maxmind.com"
+git config --global user.name "Dallas Hogan"
+
+git config --global alias.b 'branch'
+git config --global alias.ca 'commit --amend'
+git config --global alias.can 'commit --amend --no-edit'
+git config --global alias.ct 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.dc 'diff --cached'
 git config --global alias.doms 'diff -w -M origin/main...HEAD --stat --name-only'
+git config --global alias.prom 'pull --rebase origin master'
+git config --global alias.pf 'push --force-with-lease'
+git config --global alias.rsh 'reset --soft HEAD^'
+git config --global alias.st 'status'
