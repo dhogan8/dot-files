a8a691ac53407ffeccf443fada9ee064e4185d3c
Dallas Hogan <dhogan@maxmind.com>
2022-05-16 15:06:31 -0400

    Add spooninstall.sh

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -172,8 +172,6 @@ local gmail = "https://mail.google.com/mail/u/0/"
 local ircCloud = "https://www.irccloud.com/irc/"
 local PT = "https://www.pivotaltracker.com"
 
-hs.hotkey.bind(my_hotkeys, "a", cpanAuthorLink)
-hs.hotkey.bind(my_hotkeys, "b", cpanDocumentationLink)
 hs.hotkey.bind(my_hotkeys, "c", nil, open_app_action("Google Chrome"))
 hs.hotkey.bind(my_hotkeys, "g", nil, chrome_tab_action(gmail, gmail .. "#inbox"))
 hs.hotkey.bind(my_hotkeys, "i", nil, open_app_action("wezterm"))
@@ -184,12 +182,9 @@ hs.hotkey.bind(my_hotkeys, "o", nil, chrome_tab_action(ircCloud, ircCloud .. "ma
 hs.hotkey.bind(my_hotkeys, "p", nil, chrome_tab_action(PT, PT))
 hs.hotkey.bind(my_hotkeys, "q", slackifyName)
 
-hs.hotkey.bind(hyper, "a", cpanAuthorLink)
-hs.hotkey.bind(hyper, "b", cpanDocumentationLink)
 hs.hotkey.bind(hyper, "c", nil, open_app_action("Google Chrome"))
 hs.hotkey.bind(hyper, "g", nil, chrome_tab_action(gmail, gmail .. "#inbox"))
 hs.hotkey.bind(hyper, "i", nil, open_app_action("wezterm"))
-hs.hotkey.bind(hyper, "k", xpasswd)
 hs.hotkey.bind(hyper, "l", nil, open_app_action("Slack"))
 hs.hotkey.bind(hyper, "m", nil, chrome_tab_action("https://meet.google.com/", ""))
 -- hs.hotkey.bind(hyper, "n", nil, chrome_tab_action(github, github))
---
+++ spooninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+cd /tmp || exit 1
+
+FILE=SpoonInstall.spoon.zip
+
+rm -f $FILE
+rm -rf SpoonInstall.spoon
+curl --location -O "https://github.com/Hammerspoon/Spoons/raw/master/Spoons/$FILE"
+open $FILE
