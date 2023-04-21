987c07187a497f0f7de9b58d6dec64b89a9d31f4
Dallas Hogan <dallastar4@gmail.com>
2023-04-21 12:30:16 -0400

    Add vagrant

--- install.sh
+++ install.sh
@@ -53,6 +53,7 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install bash-completion
   brew install cpm
   brew install openssl
+  brew install hashicorp/tap/hashicorp-vagrant
 else
   FILE=oh-my-posh
   cd /tmp || exit
