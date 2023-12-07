7fbf0c3a931fffffd8e365f9cb5bfd8181502e19
Dallas Hogan <dallastar4@gmail.com>
2023-12-07 13:12:54 +0000

    Add ubi

---
+++ installer/ubi.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -eux
+
+in="$HOME/local/bin"
+mkdir -p "$in"
+
+if [[ ! "$(command -v curl)" && "$(command -v apt-get)" ]]; then
+    if [[ ! "$(command -v sudo)" ]]; then
+        apt-get update && apt-get install sudo --autoremove -y
+    fi
+    apt-get install curl --autoremove -y
+fi
+
+if [ ! "$(command -v "$in/ubi")" ]; then
+    curl --silent --location \
+        https://raw.githubusercontent.com/houseabsolute/ubi/master/bootstrap/bootstrap-ubi.sh |
+        TARGET=$in sh
+
+else
+    if is cli age "$in/ubi" gt 7 days; then
+        "$in/ubi" --self-upgrade
+    fi
+fi
+
+maybe_install() {
+    local project
+    local repo
+    IFS='/' read -r project repo <<<"$1"
+    if ! is there "$repo" || is cli age "$repo" gt 7 days; then
+        ubi --project "$project/$repo" --in "$in"
+    fi
+}
+
+maybe_install crate-ci/typos
+maybe_install dandavison/delta
+maybe_install houseabsolute/omegasort
+maybe_install houseabsolute/precious
+maybe_install jqlang/jq
+maybe_install junegunn/fzf
+maybe_install oalders/is
+
+if is os name eq darwin; then
+    if is cli version bat ne 0.24.0 || true; then
+        ubi --in "$in" \
+            --url https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-apple-darwin.tar.gz
+    fi
+else
+    ubi --project sharkdp/bat --in "$in"
+fi
+
+if ! is there gh || is cli age gh gt 7 days; then
+    ubi --project cli/cli --in "$in" --exe gh
+fi
+
+exit
